f083beb155dc0de9657d4809942045d7fd9e1841
jletard <johnlt77@gmail.com>
2022-04-30 07:36:39 -0600

    Working better

--- todo.js
+++ todo.js
@@ -26,29 +26,26 @@ class Task {
 
 let users = [];
 
-function createUser(user) {
-    fetch(url, {
+async function createUser(user) {
+    await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('SUCCESS', data);
+    })
+    .catch((error) => {
+        console.error('ERROR', error);
     });
 }
 
-function drawDropDown() {
-    let htmlCode = `<select class="custom-select my-1 mr-sm-2" id="userDropDown">
-            `
-    for (let i = 0; i < users.length; i++) {
-        htmlCode += `<option value="${i}" id="user${i}">${users[i].name}</option>
-                `;
-    }
-    htmlCode += `</select>`;
-    $("#user-dropdown").append(htmlCode);
-}
-
-function getAllUsers() {
-    fetch(url)
+async function getAllUsers() {
+    users = [];
+    await fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log('success', data);
@@ -59,99 +56,109 @@ function getAllUsers() {
         })
 }
 
-function deleteUser(id) {
-    fetch(url + `/${id}`, {
+async function deleteUser(id) {
+    await fetch(url + `/${id}`, {
         method: 'DELETE'
     })
 }
 
-function updateUser(user) {
-    return $.ajax({
-        url: url + `/${user._id}`,
-        dataType: 'json',
-        data: JSON.stringify(user),
-        contentType: 'application/json',
-        type: 'PUT'
+async function updateUser(user) {
+    await fetch((`${url}/${user._id}`), {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('SUCCESS', data);
+    })
+    .catch((error) => {
+        console.error('ERROR', error);
     });
 }
 
 
 // Aaron
-class DOMManager {
-    static users;
-    static createUser(user) {
-        UserService.createUser(new User(name, parent))
-            .then(() => {
-                return UserService.getAllUsers();
-            })
-            .then((users) => this.render(users));
-    }
+// class DOMManager {
+//     static users;
+//     static createUser(user) {
+//         UserService.createUser(new User(name, parent))
+//             .then(() => {
+//                 return UserService.getAllUsers();
+//             })
+//             .then((users) => this.render(users));
+//     }
 
-    // Derin - added few more lines to aarons render code to add in all the new task columns
-    static render(users) {
-        this.users = users;
-        $('#master-table-body').empty();
-        //if (users.parent = true) Want to do this for parent users to render delete button ${user.parent == true}
-        for (let user of users) {
-            console.log('building task table');
-            $('#master-table-body').append(
-                `<tr>
-                    <td id="${user._id}">${user.name}</td>
-                    <td>${user.task.description}</td>
-                    <td>${user.task.timeAssigned}</td>  
-                    <td>${user.task.dueTime}</td>
-                    <td id="${user._id}-check">
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="">
-                        </div>
-                    </td>
-                </tr> `
-            );
-        }
+//     // Derin - added few more lines to aarons render code to add in all the new task columns
+//     static render(users) {
+//         this.users = users;
+//         $('#master-table-body').empty();
+//         //if (users.parent = true) Want to do this for parent users to render delete button ${user.parent == true}
+//         for (let user of users) {
+//             console.log('building task table');
+//             $('#master-table-body').append(
+//                 `<tr>
+//                     <td id="${user._id}">${user.name}</td>
+//                     <td>${user.task.description}</td>
+//                     <td>${user.task.timeAssigned}</td>  
+//                     <td>${user.task.dueTime}</td>
+//                     <td id="${user._id}-check">
+//                         <div class="form-check">
+//                         <input class="form-check-input" type="checkbox" value="">
+//                         </div>
+//                     </td>
+//                 </tr> `
+//             );
+//         }
+//     }
+// }
+
+function drawDropDown() {
+    $("#user-dropdown").empty();
+    let htmlCode = `<select class="custom-select my-1 mr-sm-2" id="userDropDown">
+            `
+    for (let i = 0; i < users.length; i++) {
+        htmlCode += `<option value="${i}" id="user${i}">${users[i].name}</option>
+                `;
     }
+    htmlCode += `</select>`;
+    $("#user-dropdown").append(htmlCode);
 }
 
-// DOMManager.getAllUsers();
-
-// $('add-task').on('click', () => {
-//     let newTask = 
-// })
-
-
-
-
-
-
-
-
-// This is temporary and was used for testing the add user to the table.
 //Derin - added a few more rows to add in the Task description, Time assigned, time due, etc
 let masterTable = $('#master-table-body');
 let taskArray = [];
 
 $('#add-task').on('click', function () {
-    let user = $('#userDropDown').find(":selected").text();
+    let userIndex = $('#userDropDown').find(":selected").val();
     let task = $('#input-task-description').val();
     let timestart = new Date().toLocaleString();
     let timedue = $('#input-task-due-time').val();
     timedue = timedue.toLocaleString('en-US', {hour12: true});
     let newTask = new Task(task, timestart, timedue);
-    taskArray.push(newTask);
-    masterTable.append(
-        `<tr>
-            <td>${user}</td>
-            <td>${task}</td>
-            <td>${timestart}</td>
-            <td>${timedue}</td>
-            <td>
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="">
-                <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="${user}.deleteTask(${newTask})">Delete Task</button>
-                </div>
-            </td>
-        </tr> `);
-    console.log(taskArray);
-    console.log(taskArray[0].description);
+    users[userIndex].tasks.push(newTask);
+    updateUser(users[userIndex]);    
+    buildTable();
+
+    //The below code is handwaving, should be replaced with the user update and just drawing the table again.
+    // taskArray.push(newTask);
+    // masterTable.append(
+    //     `<tr>
+    //         <td>${user}</td>
+    //         <td>${task}</td>
+    //         <td>${timestart}</td>
+    //         <td>${timedue}</td>
+    //         <td>
+    //             <div class="form-check">
+    //             <input class="form-check-input" type="checkbox" value="">
+    //             <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="${user}.deleteTask(${newTask})">Delete Task</button>
+    //             </div>
+    //         </td>
+    //     </tr> `);
+    // console.log(taskArray);
+    // console.log(taskArray[0].description);
 });
 
 
