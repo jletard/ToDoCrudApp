ce2606847f0265392fa7cf408f600dab1f0a8818
smstalker <sarah.stalker15@outlook.com>
2022-04-29 22:20:46 -0600

    most current code

--- todo.js
+++ todo.js
@@ -10,6 +10,11 @@ class User {
     assignTask(task) {
         this.tasks.push(task);
     }
+
+    deleteTask(task) {
+        this.tasks.splice(this.tasks.indexof(task), 1);
+    }
+
 }
 
 class Task {
@@ -21,26 +26,34 @@ class Task {
     }
 }
 
-let users =[];
+let users = [];
 
 function createUser(user) {
     fetch(url, {
         method: 'POST',
-            headers: {
-            'Content-Type' : 'application/json',
-    },
-    body: JSON.stringify(user)
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)
     });
 }
 
 function getAllUsers() {
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log('success', data);
-        users = data;
-        return users;
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('success', data);
+            users = data;
+            let htmlCode = `<select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            `
+            for (let i = 0; i < users.length; i++) {
+                htmlCode += `<option value="${i}">${users[i].name}</option>
+                `;
+            }
+            htmlCode += `</select>`;
+            $("#user-dropdown").append(htmlCode);
+            return users;
+        })
 }
 
 // function deleteUser(id) {
@@ -120,11 +133,11 @@ class DOMManager {
 let masterTable = $('#master-table-body');
 let taskArray = [];
 
-$('#add-task').on('click',function(){
-    let user= $('#inlineFormCustomSelectPref').html();
-    let task= $('#input-task-description').val();
-    let timestart=$('#input-task-time').val();
-    let timedue=$('#input-task-due-time').val();
+$('#add-task').on('click', function () {
+    let user = $('#inlineFormCustomSelectPref').html();
+    let task = $('#input-task-description').val();
+    let timestart = $('#input-task-time').val();
+    let timedue = $('#input-task-due-time').val();
     let newTask = new Task(task, timestart, timedue);
     taskArray.push(newTask);
     masterTable.append(
@@ -136,57 +149,58 @@ $('#add-task').on('click',function(){
             <td>
                 <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="">
-                <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="DOMManager.deleteTask('${user._id}', '${task._id}')">Delete Task</button>
+                <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="${user}.deleteTask(${newTask})">Delete Task</button>
                 </div>
             </td>
         </tr> `);
-        console.log(taskArray);
-        console.log(taskArray[0].description);
+    console.log(taskArray);
+    console.log(taskArray[0].description);
 });
 
 
-$('#sort').on('click',function(){
+$('#sort').on('click', function () {
     var column = $(this).data('column');
     var order = $(this).data('order');
     var text = $(this).html();
-    text = text.substring(0, text.length -1)
+    text = text.substring(0, text.length - 1)
 
     console.log('Column was clicked', column, order);
-    
-    if(order == 'desc'){
+
+    if (order == 'desc') {
         $(this).data('order', "asc");
         // sort method
-        taskArray = taskArray.sort((a,b) => a[column] > b[column] ? 1:-1);
+        taskArray = taskArray.sort((a, b) => a[column] > b[column] ? 1 : -1);
         text += '&#9660';
     }
-    else{
+    else {
         $(this).data('order', "desc");
-        taskArray = taskArray.sort((a,b) => a[column] < b[column] ? 1:-1);
+        taskArray = taskArray.sort((a, b) => a[column] < b[column] ? 1 : -1);
         text += '&#9650';
     }
     $(this).html(text);
     buildTable(taskArray);
 });
 
-function buildTable(data){
+function buildTable(data) {
     masterTable.empty();
-    for (let i = 0; i < data.length; i++) {
-        // the User name rebuilds as i, since we don't have that working yet.
-        masterTable.append(`<tr>
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < users[i].tasks.length; j++)
+            // the User name rebuilds as i, since we don't have that working yet.
+            masterTable.append(`<tr>
                         <td>${i}</td>
-                        <td>${data[i].description}</td>
-                        <td>${data[i].timeAssigned}</td>
-                        <td>${data[i].dueTime}</td>
+                        <td>${users[i].tasks[j].description}</td>
+                        <td>${users[i].tasks[j].timeAssigned}</td>
+                        <td>${users[i].tasks[j].dueTime}</td>
                         <td>
                             <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="">
-                            <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="DOMManager.deleteTask('${data._id}')">Delete Task</button>
+                            <input class="form-check-input" type="checkbox" value=""><button class="btn btn-danger" onclick="${users[i]}.deleteTask(${users[i].tasks[j]})">Delete Task</button>
                             </div>
                         </td>
                     </tr> `
-        )
-        
-    } 
+            )
+
+    }
 }
 // Aaron
 
@@ -194,7 +208,7 @@ function buildTable(data){
 
 //Derin - Function to hide/show adult forms on radio click
 $('#adult-form').hide()
-$('input[type="radio"]').on('click', function(){
+$('input[type="radio"]').on('click', function () {
     //show parent div when user-parent selected
     console.log(this);
     if ($(this).attr('id') == 'user-parent') {
